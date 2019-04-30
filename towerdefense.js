10dc143cc48db441e2f00b471893520be067b363
yogiduzit <yverma2@my.bcit.ca>
2019-04-29 22:20:05 -0700

    Added path

--- towerdefense.js
+++ towerdefense.js
@@ -16,13 +16,36 @@ var game = new Phaser.Game(config);
 var graphics;
 var path;
 
+/** 
+ * Loads the game assets like enemies and turrets
+ */ 
 function preload() {
-  // Loads the game assets like enemies and turrets
-}
-function create() {
-  // Create the game logic here.
   
+  // Loads the atlas/directory of all the available sprites along with their JSON attributes.
+  this.load.atlas('sprites', 'assets/spritesheet.png', 'assets/spritesheet.json');
+
+  //Loads an image which can be reference by the name 'bullet'
+  this.load.image('bullet', 'assets/bullet.png');
 }
+
+/**
+ * Create the game logic here.
+ */
+function create() {
+  var graphics = this.add.graphics();
+
+  path = this.add.path(96, -32);
+  path.lineTo(96, 164);
+  path.lineTo(480, 164);
+  path.lineTo(480, 544);
+
+  graphics.lineStyle(3, 0xffffff, 1);
+
+  path.draw(graphics);
+}
+/**
+ * Updates the game every frame to make sure that all the events are taken care of.
+ */
 function update() {
-  // Updates the game every frame to make sure that all the events are taken care of.
+
 }
