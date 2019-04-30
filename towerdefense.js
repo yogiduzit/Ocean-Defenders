666d0f7897085b40f9916072cfdf181c1378ffe3
yogiduzit <yverma2@my.bcit.ca>
2019-04-29 22:01:25 -0700

    Added basic functions and framework

---
+++ towerdefense.js
@@ -0,0 +1,28 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: content,
+  width: 640,
+  height: 512,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+
+var graphics;
+var path;
+
+
+function preload() {
+  // Loads the game assets like enemies and turrets
+}
+function create() {
+  // Create the game logic here.
+  
+}
+function update() {
+  // Updates the game every frame to make sure that all the events are taken care of.
+}
