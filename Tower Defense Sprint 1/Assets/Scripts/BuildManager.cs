19918bd18f9c699c65d90c3b9889ddff7312e575
Herbert Ma <hma49@sfu.ca>
2019-05-06 14:32:23 -0700

    Updated enemies and UI

--- Tower Defense Sprint 1/Assets/Scripts/BuildManager.cs
+++ Tower Defense Sprint 1/Assets/Scripts/BuildManager.cs
@@ -1,36 +1,74 @@
-﻿
 using UnityEngine;
 
-public class BuildManager : MonoBehaviour
-{
-
+public class BuildManager : MonoBehaviour {
+    public CameraShake camerashake;
+    public Vector3 positionOffset;
     public static BuildManager instance;
-    private GameObject turretToBuild;
+    private TurretBluePrint turretToBuild;
 
-    public GameObject standardTurretPrefab;
+    //B
+    private GameObject toBuild;
 
-    private void Awake()
-    {
-        if(instance != null)
-        {
+    public bool CanBuild { get { return turretToBuild != null; } } // A C# Property 
 
-            Debug.Log("Build Manager has already been instantiated. ERROR: There should only be one Build Manager within the game.");
-        }
-        else
-        {
+    private void Start () { }
+    private void Awake () {
+        if (instance != null) {
+
+            Debug.Log ("Build Manager has already been instantiated. ERROR: There should only be one Build Manager within the game.");
+        } else {
 
             instance = this; // Else set the instance of the build manager to this object. 
         }
     }
 
-    public GameObject GetTurretToBuild()
-    {
-        return turretToBuild;
+    public void SelectTurretToBuild (TurretBluePrint turret) {
+
+        Debug.Log (turret.cost);
+        Debug.Log (turret.prefab);
+        turretToBuild = turret;
     }
 
-    private void Start()
-    {
-        turretToBuild = standardTurretPrefab;
+    public void BuildTurretOn (Node node) {
+        if (PlayerStats.Money < turretToBuild.cost) {
+            Debug.Log ("Not enough money to build, you only have " + PlayerStats.Money);
+            return;
+        } else {
+            PlayerStats.Money -= turretToBuild.cost;
+
+            GameObject turret = (GameObject) Instantiate (turretToBuild.prefab, node.GetBuildPosition (), Quaternion.identity);
+            turret.name = turretToBuild.name; // Set the name of the created game object to the blueprint name
+            node.turret = turret;
+            Debug.Log ("you spent: " + turretToBuild.cost + " and have " + PlayerStats.Money + " left");
+            turretToBuild = null; // Once you have purchased a turret you have to press the correct button to rebuild. 
+            StartCoroutine (camerashake.Shake (0.10f, 0.35f));
+        }
+
+    }
+    void AddMoney () {
+        if (turretToBuild.money != 0) {
+            PlayerStats.Money += turretToBuild.money;
+        }
+    }
+
+    public bool hasMoney {
+        get {
+            if (turretToBuild != null) {
+                return PlayerStats.Money >= turretToBuild.cost;
+            } else {
+                return false;
+            }
+        }
+
+    }
+    public bool hasSelected {
+        get {
+            if (turretToBuild != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
 }
---
+++ Tower Defense Sprint 1/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+   // public GameObject enemy;
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    // The time between different waves
+    public float waveTime = 5f;
+
+    // Time for first wave
+    private float countdown = 2f;
+
+    //Number of enemies
+    private int waveIndex = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //clone = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = waveTime;
+
+
+        }
+        countdown -= Time.deltaTime;
+    }
+    IEnumerator SpawnWave() { 
+    waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    { 
+        if (enemyPrefab != null)
+        {
+            // Instantiates new enemies at a certain position every second.
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        }
+    }
+}
