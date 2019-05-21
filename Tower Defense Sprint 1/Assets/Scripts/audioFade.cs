fbb7967139854a1323f65d73fa50bb8f9c1b517d
RoyArka <blueroy9@live.com>
2019-05-20 23:34:32 -0700

    added new script that allows source audio to fade in.

---
+++ Tower Defense Sprint 1/Assets/Scripts/audioFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class audioFade : MonoBehaviour
+{
+    [SerializeField]
+    private int m_FadeInTime = 10;
+    private AudioSource m_AudioSource;
+
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void Update()
+    {
+        if (m_AudioSource.volume < 1)
+        {
+            m_AudioSource.volume = m_AudioSource.volume + (Time.deltaTime / (m_FadeInTime + 1));
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
