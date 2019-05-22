3045c48b971eeb32760d32f1769712ebcf629096
RoyArka <blueroy9@live.com>
2019-05-21 18:47:53 -0700

    adjusted fact randomizer script.

--- Tower Defense Sprint 1/Assets/Scripts/FactRandom.cs
+++ Tower Defense Sprint 1/Assets/Scripts/FactRandom.cs
@@ -6,20 +6,21 @@ public class FactRandom : MonoBehaviour
 {
     public Text myText;
 
-    //List of random facts.
     public string[] randomFact =
     {
-    "Around 70% of industrial waste is dumped to water.",
+    "Around 70% of industrial waste is dumped into our oceans.",
     "80% of the water pollution is caused due to domestic sewage.",
-    "More than 6 billion pounds of garbage end up in the oceans every year.",
-    "Almost two million tons of human waste are exposed daily to water.",
+    "More than 6 billion pounds of garbage end up in our oceans every year.",
+    "Almost two million tons of human waste are exposed daily to our oceans.",
     "The nuclear crisis created by the tsunami of 2011, unleashed 11 million liters of radioactive water into the Pacific Ocean.",
-    "On average 250 million people succumb each year from diseases caused by the contaminated water.",
-    "40 percent of plastic produced is packaging, used just once and then discarded.",
+    "On average 250 million people succumb each year from diseases caused by contaminated water.",
+    "40 percent of plastic produced is packaging, used just once and then discarded ending up in our oceans.",
     "Nearly half of all plastic ever manufactured has been made since 2000.",
-    "Less than a fifth of all plastic is recycled globally.",
+    "Less than a fifth of all plastic is recycled globally, the rest ends up in our oceans.",
+    "73% of beach litter worldwide is plastic.",
+    "90% of plastic polluting our oceans is carried by just 10 rivers.",
 };
-    //Intializes and chooses random fact from array randomFact.
+
     void Start()
     {
         string myString = randomFact[Random.Range(0, randomFact.Length)];
